8965b02c842f51a682bb4dc873004d0c6258ab21
entymon <pawel.olejniczak.i@gmail.com>
2024-01-15 19:58:05 +0100

    Keep app vertically

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
@@ -11,53 +12,56 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kDarkColorScheme,
-        cardTheme: const CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkColorScheme.primaryContainer,
-            foregroundColor: kDarkColorScheme.onPrimaryContainer,
-          ),
-        ),
-      ),
-      theme: ThemeData().copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(
-                // can be ThemeData().textTheme ....
-                fontWeight: FontWeight.bold,
-                color: kColorScheme.onSecondaryContainer,
-                fontSize: 16,
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((fn) => {
+            runApp(
+              MaterialApp(
+                darkTheme: ThemeData.dark().copyWith(
+                  colorScheme: kDarkColorScheme,
+                  cardTheme: const CardTheme().copyWith(
+                    color: kDarkColorScheme.secondaryContainer,
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 8,
+                    ),
+                  ),
+                  elevatedButtonTheme: ElevatedButtonThemeData(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: kDarkColorScheme.primaryContainer,
+                      foregroundColor: kDarkColorScheme.onPrimaryContainer,
+                    ),
+                  ),
+                ),
+                theme: ThemeData().copyWith(
+                  colorScheme: kColorScheme,
+                  appBarTheme: const AppBarTheme().copyWith(
+                    backgroundColor: kColorScheme.onPrimaryContainer,
+                    foregroundColor: kColorScheme.primaryContainer,
+                  ),
+                  cardTheme: const CardTheme().copyWith(
+                    color: kColorScheme.secondaryContainer,
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 8,
+                    ),
+                  ),
+                  elevatedButtonTheme: ElevatedButtonThemeData(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: kColorScheme.primaryContainer,
+                    ),
+                  ),
+                  textTheme: ThemeData().textTheme.copyWith(
+                        titleLarge: TextStyle(
+                          // can be ThemeData().textTheme ....
+                          fontWeight: FontWeight.bold,
+                          color: kColorScheme.onSecondaryContainer,
+                          fontSize: 16,
+                        ),
+                      ),
+                ),
+                themeMode: ThemeMode.system,
+                home: const Expenses(),
               ),
-            ),
-      ),
-      themeMode: ThemeMode.system,
-      home: const Expenses(),
-    ),
-  );
+            )
+          });
 }
